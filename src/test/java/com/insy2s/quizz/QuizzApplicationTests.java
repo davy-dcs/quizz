cdfb2e0d3251064e55add0789a86f39c02570c6a
Davy Descamps <davydescamps01@gmail.com>
2025-01-22 11:51:43 +0100

    Initial commit

---
+++ src/test/java/com/insy2s/quizz/QuizzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insy2s.quizz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
